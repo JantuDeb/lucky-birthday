ef82857c5dbd5752d312c510979a4cf839acbcf7
JD <jantud54@gmail.com>
2021-10-03 01:58:57 +0530

    fix - logical error

--- main.js
+++ main.js
@@ -6,23 +6,37 @@ const textMessage = document.querySelector(".message")
 let formatedDOB;
 inputDOB.addEventListener("change", function () {
     var input = this.value;
+    console.log(input);
     formatedDOB = input.replaceAll("-", "")
+
 });
 
 buttonCheck.addEventListener('click', function () {
-    if (inputNumber.value && formatedDOB) {
+    if (formatedDOB === undefined) {
+        showError("Pick your birthday")
+    }
+    else if (inputNumber.value && inputNumber.value > 0) {
         let sum = 0;
         for (let char in formatedDOB) {
             sum += Number(formatedDOB.charAt(Number(char)));
         }
         console.log(sum % inputNumber.value === 0);
         if (sum % inputNumber.value === 0) {
+
             textMessage.style.display = "block"
-        } else {
-            textMessage.style.display = "block"
-            textMessage.innerHTML = `Opps! ${inputNumber.value} is not your lucky number`
-            textMessage.style.color = "red"
-        }
+            textMessage.style.color = "green"
+            textMessage.innerHTML = "Great! It's your lucky number"
+        } else
+            showError(`Opps! ${inputNumber.value} is not your lucky number`)
+
+    } else {
+        showError("Number should not be negative or 0");
     }
 
 })
+
+function showError(str) {
+    textMessage.style.display = "block";
+    textMessage.innerHTML = str;
+    textMessage.style.color = "red";
+}
